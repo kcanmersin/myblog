64b810ff65d92ce0dfa266a3422d9ebfb72fa543
Kerim Can Mersin <k.mersin2020@gtu.edu.tr>
2024-07-17 21:20:46 +0300

    data layer

--- Blog.Data/obj copy/Debug/net6.0/YoutubeBlog.Data.AssemblyInfo.cs
+++ Blog.Data/obj copy/Debug/net6.0/YoutubeBlog.Data.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Blog.Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+69fd365009c984ba82a86c91053ba6967f697345")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Blog.Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Blog.Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
